0319ba51a3e015650e21091fa71d29d804f9724e
River Endless <myowngollum@gmail.com>
2020-05-13 00:43:00 +0300

    Fix input data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double[] input = {1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0};
+        double[] input = {1.0, 0.9, 0.81, 0.243, 0.9, 1.0, 0.9, 0.81, 0.81, 0.9, 1.0, 0.9, 0.243, 0.81, 0.9, 1.0};
 
         Complex[] cinput = new Complex[input.length];
         for (int i = 0; i < input.length; i++)
